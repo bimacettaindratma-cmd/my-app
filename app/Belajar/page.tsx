a6f59e1819011f03bab7c5224985310215549778
Bima <bimacettaindratma@gmail.com>
2026-05-03 16:49:53 +0800

    tugas lanjutkan dirumah

---
+++ app/Belajar/page.tsx
@@ -0,0 +1,121 @@
+export default function LearnPage() {
+    return (
+    <section className="h-screen p-8  bg-white text-black flex flex-col gap-3 items-center justify-center">
+    <div className="flex gap-3"> 
+    <div className="bg-green-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-green-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Design</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">UI/UX Design</p>
+            <p className="font-bold">in 2 days</p>
+        </div>
+        <img src="/palette.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+    </div>
+    
+    <div className="bg-blue-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-blue-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Design</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">Illustrator</p>
+            <p className="font-bold">in 3 days</p>
+        </div>
+        <img src="/vector-bezier.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+    </div>
+    
+     <div className="bg-purple-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-purple-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Coding</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">JavaScript</p>
+            <p className="font-bold">in 5 days</p>
+        </div>
+        <img src="/file-barcode.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+    </div>
+    </div>
+    
+    <div className="flex gap-3">
+       <div className="bg-orange-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-orange-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Coding</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">HTML & CSS</p>
+            <p className="font-bold">1 day ago</p>
+        </div>
+        <img src="/code.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+        </div> 
+
+        <div className="bg-yellow-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-yellow-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Database</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">MySQL</p>
+            <p className="font-bold">2 days ago</p>
+        </div>
+        <img src="/cylinder.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+        </div> 
+
+        <div className="bg-red-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-red-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Design</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">Adobe XD</p>
+            <p className="font-bold">3 days ago</p>
+        </div>
+        <img src="/brand-adobe-xd.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+        </div> 
+    </div>
+
+    <div className="flex gap-3">
+        <div className="bg-cyan-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-cyan-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Coding</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">React JS</p>
+            <p className="font-bold">4 days ago</p>
+        </div>
+        <img src="/topology-star.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+        </div> 
+
+        <div className="bg-emerald-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-emerald-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Backend</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">Node.js</p>
+            <p className="font-bold">5 days ago</p>
+        </div>
+        <img src="/brand-nodejs.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+        </div> 
+
+        <div className="bg-fuchsia-100 p-4 h-48 w-80 rounded-2xl flex flex-col justify-between relative text-fuchsia-400">
+        <div className="flex gap-3">
+            <img src="/circle-check.svg" alt="design"/>
+            <p className="font-bold">Tools</p>
+        </div>
+        <div>
+            <p className="text-2xl font-bold">Git & GitHub</p>
+            <p className="font-bold">6 days ago</p>
+        </div>
+        <img src="/brand-github.svg" alt="design" className="absolute right-4 bottom-4 w-20 h-20 opacity-10"/>
+        </div> 
+    </div>
+
+    </section>
+    );
+}
+        
--- app/task-2/page.tsx
+++ app/task-2/page.tsx
@@ -1,82 +1,82 @@
-export default function LearnPage() {
+export default function LearnPge() {
     return(
-<section className="h-screen bg-gray-100 flex flex-col gap-6 p-8 justify-center items-center ">
-    <img src="circle-check.svg" className="flex self-start top-16 left-18"/>
-    <div className="flex gap-6">
-    <div className="bg-cyan-100 p-8 h-80 w-[445] rounded-4xl flex flex-col justify-between text-cyan-950">
-        <div className="flex gap-4">
-            <img src="/palette.svg" alt="design"/>
-            <p className="font-bold">Design</p>
-        </div>
-        <div>
-          <p className="text-4xl font-bold">Adobe Photoshop</p>
-          <div className="flex justify-between"> 
-            <p className="font-bold flex">in 3 days</p>
-            <div className="flex ">
-                <img src="1.png" className="rounded-full w-9 h-9 border-3 border-cyan-100"/>
-                <img src="2.png" className="rounded-full w-9 h-9 border-3 border-cyan-100 -ml-4"/>
-                <img src="3.png" className="rounded-full w-9 h-9 border-3 border-cyan-100 -ml-4"/>
-                <p className="rounded-full w-9 h-9 border-3 border-cyan-100 -ml-4 bg-cyan-300 flex items-center justify-center">9+</p>
-            </div> 
-          </div>
-        </div>
-    </div>
-
-    <div className="bg-green-100 p-8 h-80 w-[445] rounded-4xl flex flex-col justify-between text-green-950">
-        <div className="flex gap-4">
-            <img src="/terminal-2.svg" alt="design"/>
-            <p className="font-bold">AI</p>
-        </div>
-        <div>
-          <p className="text-4xl font-bold">DALL·E 2, Midjourney, Stable Diffusion</p>
-          <div className="flex justify-between">
-            <p className="font-bold">in 5 days</p>
-            <div className="flex">
-                <img src="4.png" className="rounded-full w-9 h-9 border-3 border-green-100"/>
-                <img src="5.png" className="rounded-full w-9 h-9 border-3 border-green-100 -ml-4"/>
-                <img src="6.png" className="rounded-full w-9 h-9 border-3 border-green-100 -ml-4"/>
-                <p className="rounded-full w-9 h-9 border-3 border-green-100 -ml-4 bg-green-300 flex items-center justify-center">3+</p>
-            </div>  
-          </div>
-        </div>
-    </div>
-    </div>
-
-    <div className="flex gap-6">
-        <div className="bg-purple-100 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-blue-950">
-            <div className=" flex gap-4">
-                <img src="/palette.svg" alt="design"/>
+    <section className="h-screen bg-white p-8 gap-3 text-black flex flex-col justify-center items-center">
+        <div className="flex gap-3">
+        <div className="bg-cyan-100 p-9 h-72 w-[445] rounded-3xl flex flex-col  justify-between relative text-cyan-950">
+            <div className="flex gap-3">
+                <img src="palette.svg/" alt="design"/>
                 <p className="font-bold">Design</p>
             </div>
-        <div>
-            <p className="text-4xl font-bold">Figma</p>
-            <p className="font-bold">8 hours ago</p>
+            <div>
+                <p className="text-3xl font-bold">Adobe Photoshop</p>
+                <p className="font-bold">in 3 days</p>
+            </div>
+            <div className="absolute bottom-7 right-6">
+            <div className="flex ">
+                <img src="1.png" className="rounded-full w-8 h-8 border-3 border-cyan-100"/>
+                <img src="2.png" className="rounded-full w-8 h-8 border-3 border-cyan-100 -ml-3"/>
+                <img src="3.png" className="rounded-full w-8 h-8 border-3 border-cyan-100 -ml-3"/>
+                <p className="rounded-full w-8 h-8 border-3 border-cyan-100 -ml-3 bg-cyan-300 flex items-center justify-center">9+</p>
+            </div> 
+            </div>
+        </div>
+
+        <div className="bg-green-100 p-9 h-72 w-[445] rounded-3xl flex flex-col  justify-between relative text-green-950">
+            <div className="flex gap-3">
+                <img src="Terminal-2.svg/" alt="design"/>
+                <p className="font-bold">AI</p>
+            </div>
+            <div>
+                <p className="text-3xl font-bold">DALL-E 2,Midjourney, Stable Diffusion</p>
+                <p className="font-bold">in 5 days</p>
+            </div>
+            <div className="absolute bottom-6 right-5">
+            <div className="flex ">
+                <img src="1.png/" className="rounded-full w-8 h-8 border-3 border-green-100"/>
+                <img src="2.png/" className="rounded-full w-8 h-8 border-3 border-green-100 -ml-3"/>
+                <img src="3.png/" className="rounded-full w-8 h-8 border-3 border-green-100 -ml-3"/>
+                <p className="rounded-full w-8 h-8 border-3 border-green-100 bg-green-300 -ml-3 flex items-center justify-center">3+</p>
+            </div> 
+            </div>
         </div>
         </div>
 
-        <div className="bg-orange-200 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-orange-950">
-            <div className=" flex gap-4">
-                <img src="/code.svg" alt="design"/>
+        <div className="flex gap-3">
+        <div className="bg-purple-100 p-9 h-72 w-73 rounded-3xl flex flex-col justify-between text-purple-950">
+            <div className="flex gap-3">
+                <img src="palette.svg/" alt="design"/>
+                <p className="font-bold">Design</p>
+            </div>
+            <div>
+                <p className="font-bold text-3xl">Figma</p>
+                <p className="font-bold">8 hours ago</p>
+            </div>
+        </div>
+
+        <div className="bg-orange-100 p-9 h-72 w-73 rounded-3xl flex flex-col justify-between text-orange-950">
+            <div className="flex gap-3">
+                <img src="code.svg/"/>
                 <p className="font-bold">Codding</p>
             </div>
-        <div>
-            <p className="text-4xl font-bold">Python</p>
-            <p className="font-bold">2 days ago</p>
-        </div>
+            <div>
+                <p className="text-3xl font-bold">Python</p>
+                <p className="font-bold">2 days ago</p>
+            </div>
         </div>
 
-        <div className="bg-red-200 p-8 h-72 w-72 rounded-4xl flex flex-col justify-between text-red-950">
-            <div className=" flex gap-4">
-                <img src="/palette.svg" alt="design"/>
+        <div className="bg-red-100 p-9 h-72 w-73 rounded-3xl flex flex-col justify-between text-red-950">
+            <div className="flex gap-3">
+                <img src="palette.svg/"/>
                 <p className="font-bold">Design</p>
             </div>
-        <div>
-            <p className="text-4xl font-bold">Sketch</p>
-            <p className="font-bold">4 days ago</p>
+            <div>
+                <p className="text-3xl font-bold">Sketch</p>
+                <p className="font-bold">4 days ago</p>
+            </div>
         </div>
         </div>
-        </div>
-        <p className="flex self-end fixed bottom-12 right-18 text-gray-400 font-bold">Day 020 / 365</p>
-        </section>
-    );
+        <p className="absolute bottom-10 right-7 font-bold text-gray-300">Day 020 / 365</p>
+    </section>
+        
+    )
 }
